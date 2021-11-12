bc21a7bb41f5e42ba3bfc4c6928808d668ebeb30
anjp <anjp@itu.dk>
2021-11-12 13:57:44 +0100

    Created projects
    Co-authored-by: DaneilLLind <DaneilLLind@users.noreply.github.com>
    Co-authored-by: Markusgp <markusgp@live.dk>
    Co-authored-by: linedejgaard <linedejgaard@users.noreply.github.com>

---
+++ Core/ProjectDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Core;
+
+    public record ProjectDTO();
+
+    public record ProjectListDTO() : ProjectDTO();
+    public record ProjectDetailsDTO() : ProjectDTO();
+    public record ProjectCreateDTO {
+
+    }
+
+    public record ProjectUpdateDTO : ProjectDTO {
+
+    }
+
