92d382474be10d32301e63aed11622469250fd85
Markusgp <markusgp@live.dk>
2021-11-18 15:47:34 +0100

    Started implementing tests

---
+++ Infrastructure.Tests/GlobalUsings.cs
@@ -0,0 +1,9 @@
+global using System;
+global using Microsoft.Data.Sqlite;
+global using Microsoft.EntityFrameworkCore;
+global using Xunit;
+global using System.Collections.Generic;
+global using System.Threading.Tasks;
+global using System.Linq;
+global using Core;
+global using PBInfrastructure;
