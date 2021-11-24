541537c0db5d1022b0747eb8d7e3e14608b51368
Markusgp <markusgp@live.dk>
2021-11-24 20:34:18 +0100

    Changed ICollection<int> to <Project>

--- Infrastructure/Student.cs
+++ Infrastructure/Student.cs
@@ -7,6 +7,16 @@ public class Student {
     [StringLength(50)]
     public string Name {get; set;}
 
-    public ICollection<int>? Projects {get; set;}
+    public ICollection<Project> Projects {get; set;}
 
+    public ICollection<int> GetProjectIDs() {
+        ICollection<int> ProjectIDList = new List<int>();
+        if(Projects != null) {
+            foreach (Project p in Projects) 
+            {
+            ProjectIDList.Add(p.Id);
+            }
+        }
+        return ProjectIDList;
+    }
 }
