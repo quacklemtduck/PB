dfcc077499fa489823930906fed549c0a854d1b1
Daniel <dlli@itu.dk>
2021-11-29 16:44:09 +0100

    Updated application and supervisor

--- Infrastructure/SupervisorRepository.cs
+++ Infrastructure/SupervisorRepository.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Options;
 namespace PB.Infrastructure
 {
 
@@ -16,8 +17,6 @@ namespace PB.Infrastructure
             var sup = new Supervisor{
                 Name = supervisor.Name,
                 Email = supervisor.Email,
-                Password = supervisor.Password,
-                ContactInfo = supervisor.ContactInfo,
                 Projects = new List<Project>()
             };
 
@@ -25,28 +24,27 @@ namespace PB.Infrastructure
             
             await _context.SaveChangesAsync();
 
-            return new SupervisorDetailsDTO(sup.Id,sup.Name,sup.Email,sup.ContactInfo,new List<int>());
+            return new SupervisorDetailsDTO(sup.Id,sup.Name,sup.Email,new List<int>());
         }
         public async Task<IReadOnlyCollection<SupervisorDetailsDTO>> ReadAllAsync()
         {
             return await _context.Supervisors
-                .Select(s => new SupervisorDetailsDTO(s.Id,s.Name,s.Email,s.ContactInfo,s.Projects.Select(p => p.Id).ToList<int>()))
+                .Select(s => new SupervisorDetailsDTO(s.Id,s.Name,s.Email,s.Projects.Select(p => p.Id).ToList<int>()))
                 .ToListAsync();
         }
 
         public async Task<SupervisorDetailsDTO> ReadAsync(int supervisorId)
         {
             var supervisor = await _context.Supervisors.FirstOrDefaultAsync(s => s.Id == supervisorId);
-            return supervisor == null ? null : new SupervisorDetailsDTO(supervisor.Id,supervisor.Name,supervisor.Email,supervisor.ContactInfo,supervisor.Projects.Select(p => p.Id).ToList<int>());
+            return supervisor == null ? null : new SupervisorDetailsDTO(supervisor.Id,supervisor.Name,supervisor.Email,supervisor.Projects.Select(p => p.Id).ToList<int>());
         }
-
+        
         public async Task<Response> UpdateAsync(int id, SupervisorUpdateDTO supervisor)
         {
             var entity = _context.Supervisors.FirstOrDefault(s => s.Id == id);
             if (entity == null) return NotFound;
             
             entity.Name = supervisor.Name;
-            entity.ContactInfo = supervisor.ContactInfo;
             await _context.SaveChangesAsync();
             return Updated;
         }
